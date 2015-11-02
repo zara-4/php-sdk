5db2a40ca77bc9c3186dca4e62d42a351f739325
Colin Stannard <colin.stannard@cbsindustries.com>
2015-11-02 04:56:22 +0000

    Change Image optimise ro use array of params rather than individual args

--- Zara4/API/ImageProcessing/Image.php
+++ Zara4/API/ImageProcessing/Image.php
@@ -14,89 +14,34 @@ class Image {
 
 
 
-  public static function optimiseImageFromUrl($url, $accessToken = null) {
+  public static function optimiseImageFromUrl($url, array $params = []) {
 
   }
-
-
-/*
- *       file.additionalData["output-format"] = currentOutputFormat();
-      file.additionalData["optimisation-mode"] = currentOptimisationMode();
-      file.additionalData["width"] = currentResizeWidth();
-      file.additionalData["height"] = currentResizeHeight();
-      file.additionalData["resize-mode"] = currentResizeMode();
- */
 
 
   /**
    * Optimise the image at the given file path.
    *
    * @param $filePath
-   * @param string $outputFormat
-   * @param string $optimisationMode
-   * @param string $resizeMode
-   * @param null $width
-   * @param null $height
-   * @param null|string $accessToken
+   * @param array $params
    * @return array
    */
-  public static function optimiseImageFromFile(
-    $filePath, $outputFormat = "auto", $optimisationMode = "highest", $resizeMode = "none",
-    $width = null, $height = null, $accessToken = null
-  ) {
+  public static function optimiseImageFromFile($filePath, array $params = []) {
 
     //
-    // Construct data containing file to be processed.
+    // Construct data containing file to be processed and params.
     //
-    $data = [
-      "multipart" => [
-        [
-          "name"     => "file",
-          "contents" => fopen($filePath, "r"),
-          "filename" => "test.jpg",
-        ],
-        [
-          "name"     => "output-format",
-          "contents" => $outputFormat,
-        ],
-        [
-          "name"     => "optimisation-mode",
-          "contents" => $optimisationMode,
-        ],
-        [
-          "name"     => "resize-mode",
-          "contents" => $resizeMode,
-        ],
-        [
-          "name"     => "output-format",
-          "contents" => $outputFormat,
-        ],
+    $data = ["multipart" => [
+      [
+        "name"     => "file",
+        "contents" => fopen($filePath, "r"),
+        "filename" => "test.jpg",
       ]
-    ];
-
-    //
-    // Attach dimensions
-    //
-    if($width) {
+    ]];
+    foreach($params as $key => $value) {
       $data["multipart"][] = [
-        "name"     => "width",
-        "contents" => $width
-      ];
-    }
-    if($height) {
-      $data["multipart"][] = [
-        "name"     => "height",
-        "contents" => $height
-      ];
-    }
-
-    //
-    // If we have an access_token attach it to the request (otherwise API will perform anonymous request).
-    //
-    if($accessToken) {
-      $data["multipart"][] = [
-        "name"     => "access_token",
-        "contents" => $accessToken
+        "name"     => $key,
+        "contents" => $value
       ];
     }
 
